e1e1b49941893fe068c530673fb185e68a2ddb3e
uysolanki <uysolanki@gmail.com>
2025-08-21 10:44:25 +0530

    day2

--- src/com/excelr/app/MenuDriver.java
+++ src/com/excelr/app/MenuDriver.java
@@ -15,8 +15,32 @@ public class MenuDriver {
 		
 		switch(choice)
 		{
-		case 1 :  
+		case 1 : 
+			double radius,result;
+			
+			System.out.println("Please enter radius of Cicle"); //   5.5
+			radius=sc.nextDouble();
+			
+			result=3.14*radius*radius;
+			
+			System.out.println("Area of Circle is "+result); //94.98
+			
+		
+		break;
 		case 2 : 
+			double length,width;
+			
+			System.out.println("Please enter length of Rectangle"); //   5.5
+			length=sc.nextDouble();
+			
+			System.out.println("Please enter width of Rectangle"); //   5.5
+			width=sc.nextDouble();
+			
+			result=length*width;
+			
+			System.out.println("Area of Rectangle is "+result); //94.98
+		
+			break;
 		default : System.out.println("Invalid Input");
 		}
 

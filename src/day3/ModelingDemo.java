2ae86364a09eb8a1daed62e276f6f453caa1301a
uysolanki <uysolanki@gmail.com>
2025-08-22 10:38:57 +0530

    day3

---
+++ src/day3/ModelingDemo.java
@@ -0,0 +1,35 @@
+package day3;
+
+import java.util.Scanner;
+
+public class ModelingDemo {
+
+	public static void main(String[] args) {
+
+		int age;
+		double height,weight;
+		
+		Scanner sc=new Scanner(System.in);  //ctrl shift o
+		
+		System.out.println("Please enter your age in years");
+		age=sc.nextInt();
+		
+		System.out.println("Please enter your height in feet");
+		height=sc.nextDouble();
+		
+		System.out.println("Please enter your weight in kg");
+		weight=sc.nextDouble();
+		//  T     &&     T       &&         T
+		// 21<=23      6.3>=6.1        65<=70
+		if(age<=23 && height>=6.1 && weight<=70)
+		{
+			System.out.println("Eligible for Modeling");
+		}
+		else
+		{
+			System.out.println("Not Eligible for Modeling");
+		}
+
+	}
+
+}

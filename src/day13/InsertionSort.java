f293e9fcb50cb590ea0f8a866512a8a6e3fe4a18
uysolanki <uysolanki@gmail.com>
2025-09-10 10:52:36 +0530

    insertion sort

---
+++ src/day13/InsertionSort.java
@@ -0,0 +1,27 @@
+package day13;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int arr[]= {7,5,8,6,2};
+		System.out.println("Before : "+Arrays.toString(arr));
+		for(int k=1;k<arr.length;k++)
+		{
+			int temp = arr[k];
+			int j=k-1;
+			
+			while(j>=0 && temp<=arr[j])
+			{
+				arr[j+1]=arr[j];
+				j--;
+			}
+			
+			arr[j+1]=temp;
+		}
+		System.out.println("After : "+Arrays.toString(arr));
+
+	}
+
+}

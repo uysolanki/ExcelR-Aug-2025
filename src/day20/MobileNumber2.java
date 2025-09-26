72af2a4257548fcb14aefd89f38ca0d16d41d0f7
uysolanki <uysolanki@gmail.com>
2025-09-26 10:40:50 +0530

    array mobile number

--- src/day20/MobileNumber2.java
+++ src/day20/MobileNumber2.java
@@ -2,7 +2,7 @@ package day20;
 
 public class MobileNumber2 {
 public static void main(String[] args) {
-	long arr[]= {9890131231L,8888165531L,9890145731L,9890131888L,9890131231L};
+	long arr[]= {9890131231L,8888165531L,9890145731L,9890131888L,8888831231L};
 	
 	int arr1[]= {123,456,333};
 	
@@ -26,11 +26,17 @@ public static void main(String[] args) {
 //		System.out.println("Odd Number Count "+oddCount);
 //		System.out.println("Even Number Count "+evenCount+"\n");
 		
-		System.out.println("Mobile Number " + mob+ " has " + oddCount + " Odd digits and " +evenCount +" Even digits");
+		System.out.print("Mobile Number " + mob+ " has " + oddCount + " Odd digits and " +evenCount +" Even digits");
+		if(oddCount>evenCount)
+			System.out.println(" and the number is ODD Biased");
+		else if(oddCount<evenCount)
+			System.out.println(" and the number is EVEN Biased");
+		else
+			System.out.println(" and the number is NEUTRAL");
 	}
 }
 }
 
 
 
-//Mobile Number 9890131231 has 6 Odd digits and 4 Even digits
+//Mobile Number 9890131231 has 6 Odd digits and 4 Even digits it is odd biased number , even biased , neutral

c248c597cc4b7c8613b227a77cd3b2ddc7ad900e
uysolanki <uysolanki@gmail.com>
2025-09-29 10:41:28 +0530

    right shift

---
+++ src/day21/ArrayRotate.java
@@ -0,0 +1,28 @@
+package day21;
+
+import java.util.Arrays;
+
+public class ArrayRotate {
+
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50,60};
+		System.out.println("Before Rotation "+Arrays.toString(arr));
+		
+		int mid=arr.length/2; //mid =3
+		
+		for(int i=0;i<mid;i++)				//temp 
+		{									//10
+			int temp=arr[0];
+			int j;
+				for(j=1;j<arr.length;j++)
+				{
+						arr[j-1]=arr[j];
+				}
+				
+				arr[j-1]=temp;
+		}
+		
+		System.out.println("Before Rotation at Mid "+Arrays.toString(arr));
+	}
+
+}

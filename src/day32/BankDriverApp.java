e5a4dcf513eb4e2dc03ae386e835f8b50f71c95e
uysolanki <uysolanki@gmail.com>
2025-11-04 10:45:23 +0530

    banking example

--- src/day32/BankDriverApp.java
+++ src/day32/BankDriverApp.java
@@ -1,18 +1,29 @@
 package day32;
 
+import java.util.Scanner;
+
 public class BankDriverApp {
 
 	public static void main(String[] args) {
 		
 		BankingServices bank=new HDFCBank(5000.0);
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Bank Balance is " +bank.showBalance());
+		
+		System.out.println("Enter Deposit Amount");
+		double depositAmount=sc.nextDouble();
+		if(depositAmount>=0)
+			bank.deposit(depositAmount);
 		
 		System.out.println("Bank Balance is " +bank.showBalance());
 		
-		bank.deposit(2000.0);
+		System.out.println("Enter Deposit Amount");
+		double withdrawAmount=sc.nextDouble();
+		if(withdrawAmount>=0  && withdrawAmount<=bank.showBalance())
+			bank.withdraw(withdrawAmount);
+		else
+			System.out.println("Please enter valid amount");
 		
-		System.out.println("Bank Balance is " +bank.showBalance());
-		
-		bank.withdraw(3000.0);
 		
 		System.out.println("Bank Balance is " +bank.showBalance());
 

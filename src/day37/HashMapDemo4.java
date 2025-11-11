859586aa3bc5c3ec53b2a4f7b807f5d2b2b457c2
uysolanki <uysolanki@gmail.com>
2025-11-11 11:19:41 +0530

    collection

---
+++ src/day37/HashMapDemo4.java
@@ -0,0 +1,31 @@
+package day37;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class HashMapDemo4 {
+
+	public static void main(String[] args) {
+		String sentance= "apple";
+		Map<Character,Integer> counter=new HashMap();
+		
+		for(char n:sentance.toCharArray())				
+				counter.put(n, counter.getOrDefault(n, 0)+1);
+		
+		
+		System.out.println(counter);
+	}
+
+}
+
+
+/*
+{
+one : 2
+two : 1}
+*/
